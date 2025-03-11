208b37da7859db320edc5fa7dfa0d014678e269a
aaa <shanjairajan54@gmail.com>
2025-03-11 13:04:19 +0530

    modified the code based on the url changes and params

--- app/summary/actions.ts
+++ app/summary/actions.ts
@@ -1,26 +1,25 @@
 'use server'
 
-import { revalidateTag } from 'next/cache';
 
 
-export async function getPatientData(id: string, module: string) {
+export async function getPatientData(module: string, queryString: string, mobile: string | null) {
   try {
-    if (!id.startsWith('+91')) {
-      id = `+91${id}`;
-    }
 
     const timestamp = new Date().toISOString();
     
-    console.log(`[${timestamp}] SERVER: Fetching fresh data for: ${id}, ${module}`);
+    console.log(`[${timestamp}] SERVER: Fetching fresh data for: ${mobile}, ${module}`);
+
+    const mainUrl = `http://localhost:8000/summary/${module}`
+    // const mainUrl = `https://api-personika.babymd.in/summary/${module}`
     
-    const response = await fetch(`https://api-personika.babymd.in/summary/${module}?phone_number=${id}`, {
+    const response = await fetch(`${mainUrl}?${queryString}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
       // Still use Next.js caching as a backup
       next: { 
-        tags: [`patient-${id}-${module}`],
+        tags: [`patient-${mobile}-${module}`],
         revalidate: 86400
       },
     });
